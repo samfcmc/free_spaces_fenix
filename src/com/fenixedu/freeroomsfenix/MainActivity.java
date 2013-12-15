9f1fa7e72f145b335b4c4b30b1460bf3683016b0
Samuel Coelho <samuelfcmc@gmail.com>
2013-12-15 17:29:35 +0000

    Main activity now prints the response from get spaces request to the api

--- src/com/fenixedu/freeroomsfenix/MainActivity.java
+++ src/com/fenixedu/freeroomsfenix/MainActivity.java
@@ -1,15 +1,24 @@
 package com.fenixedu.freeroomsfenix;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
+
+import com.fenixedu.freeroomsfenix.api.FenixEduAPI;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class MainActivity extends Activity {
+
+	private final FenixEduAPI api = FenixEduAPI.getInstance();
+	private TextView text;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initViews();
+		loadSpaces();
 	}
 
 	@Override
@@ -17,6 +26,25 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private void initViews() {
+		text = (TextView) findViewById(R.id.text_main);
+	}
+
+	private void loadSpaces() {
+		api.getSpaces(new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String response) {
+				text.setText(response);
+			}
+
+			@Override
+			public void onFailure(Throwable exception) {
+				text.setText(exception.getMessage());
+			}
+		});
+
 	}
 
 }
